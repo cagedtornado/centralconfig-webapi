0d549e265cc560be4ff57d83ca10de4ba0229acf
Dan Esparza <esparza.dan@gmail.com>
2018-02-08 14:51:52 -0500

    Update unit tests to call 'verify'

--- centralconfig-webapi.library.tests/ConfigDataManagerTests.cs
+++ centralconfig-webapi.library.tests/ConfigDataManagerTests.cs
@@ -12,9 +12,25 @@ namespace centralconfig_webapi.library.tests
     public class ConfigDataManagerTests
     {
         /// <summary>
+        /// Our test data
+        /// </summary>
+        private List<configitem> data = new List<configitem>
+        {
+            new configitem { id = 1, application = "*", name = "Environment", machine = "", value = "DEV", updated = DateTime.Now.AddSeconds(-12) },
+            new configitem { id = 2, application = "TestApp", name = "Environment", machine = "", value = "UNITTEST", updated = DateTime.Now.AddSeconds(-1) },
+            new configitem { id = 3, application = "TestApp", name = "AnotherItem", machine = "", value = "Some test here", updated = DateTime.Now.AddSeconds(-7) },
+            new configitem { id = 4, application = "*", name = "SomeGlobalSetting", machine = "", value = "SettingValue here", updated = DateTime.Now.AddSeconds(-12) },
+            new configitem { id = 5, application = "SomeOtherApp", name = "SpecificConfig1", machine = "Machine1", value = "Something very specific", updated = DateTime.Now.AddSeconds(-6) },
+            new configitem { id = 6, application = "SomeOtherApp", name = "SpecificConfig1", machine = "", value = "Something somewhat specific", updated = DateTime.Now.AddSeconds(-12) },
+            new configitem { id = 7, application = "SomeOtherApp", name = "AnotherConfig", machine = "", value = "Some other value", updated = DateTime.Now.AddSeconds(-8) },
+            new configitem { id = 8, application = "SomeOtherApp", name = "Environment", machine = "", value = "SPECIFIC", updated = DateTime.Now.AddSeconds(-12) },
+            new configitem { id = 9, application = "SomeOtherApp", name = "Environment", machine = "Machine1", value = "MACHINESPECIFIC", updated = DateTime.Now.AddSeconds(-9) },
+        };
+
+        /// <summary>
         /// Our returned data
         /// </summary>
-        private IQueryable<configitem> data = null;
+        private IQueryable<configitem> qdata = null;
 
         /// <summary>
         /// Our mocked dbset
@@ -29,30 +45,17 @@ namespace centralconfig_webapi.library.tests
         [TestInitialize]
         public void Test_Setup()
         {
-            //  Our test data
-            data = new List<configitem>
-            {
-                new configitem { id = 1, application = "*", name = "Environment", machine = "", value = "DEV", updated = DateTime.Now.AddSeconds(-12) },
-                new configitem { id = 2, application = "TestApp", name = "Environment", machine = "", value = "UNITTEST", updated = DateTime.Now.AddSeconds(-1) },
-                new configitem { id = 3, application = "TestApp", name = "AnotherItem", machine = "", value = "Some test here", updated = DateTime.Now.AddSeconds(-7) },
-                new configitem { id = 4, application = "*", name = "SomeGlobalSetting", machine = "", value = "SettingValue here", updated = DateTime.Now.AddSeconds(-12) },
-                new configitem { id = 5, application = "SomeOtherApp", name = "SpecificConfig1", machine = "Machine1", value = "Something very specific", updated = DateTime.Now.AddSeconds(-6) },
-                new configitem { id = 6, application = "SomeOtherApp", name = "SpecificConfig1", machine = "", value = "Something somewhat specific", updated = DateTime.Now.AddSeconds(-12) },
-                new configitem { id = 7, application = "SomeOtherApp", name = "AnotherConfig", machine = "", value = "Some other value", updated = DateTime.Now.AddSeconds(-8) },
-                new configitem { id = 8, application = "SomeOtherApp", name = "Environment", machine = "", value = "SPECIFIC", updated = DateTime.Now.AddSeconds(-12) },
-                new configitem { id = 9, application = "SomeOtherApp", name = "Environment", machine = "Machine1", value = "MACHINESPECIFIC", updated = DateTime.Now.AddSeconds(-9) },
-            }.AsQueryable();
+            //  Our queryable test data
+            qdata = new List<configitem>(data).AsQueryable();
 
             //  Wire up the DBSet operations
-            mockSet.As<IQueryable<configitem>>().Setup(m => m.Provider).Returns(data.Provider);
-            mockSet.As<IQueryable<configitem>>().Setup(m => m.Expression).Returns(data.Expression);
-            mockSet.As<IQueryable<configitem>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<configitem>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
-
+            mockSet.As<IQueryable<configitem>>().Setup(m => m.Provider).Returns(qdata.Provider);
+            mockSet.As<IQueryable<configitem>>().Setup(m => m.Expression).Returns(qdata.Expression);
+            mockSet.As<IQueryable<configitem>>().Setup(m => m.ElementType).Returns(qdata.ElementType);
+            mockSet.As<IQueryable<configitem>>().Setup(m => m.GetEnumerator()).Returns(qdata.GetEnumerator());
+            
             //  Setup the database context
             mockContext.Setup(c => c.configitems).Returns(mockSet.Object);
-
-            
         }
 
         [TestMethod]
@@ -91,14 +94,13 @@ namespace centralconfig_webapi.library.tests
         {
             //  Arrange
             ConfigDataManager manager = new ConfigDataManager(mockContext.Object);
-            List<ConfigItem> itemsBefore = manager.GetAll();    //  Get the list prior (as a baseline)
 
             //  Act            
             manager.Remove(new ConfigItem { Application = "SomeOtherApp", Name = "SpecificConfig1" });
-            List<ConfigItem> itemsAfter = manager.GetAll(); //  Get the list again to check
 
             //  Assert
-            // Assert.IsTrue(itemsBefore.Count > itemsAfter.Count);
+            mockSet.Verify(m => m.Remove(It.IsAny<configitem>()), Times.Once());
+            mockContext.Verify(m => m.SaveChanges(), Times.Once());
         }
 
         [TestMethod]
@@ -118,8 +120,8 @@ namespace centralconfig_webapi.library.tests
             var retval = manager.Set(newItem);
 
             //  Assert
-            Assert.IsNotNull(retval);
-            // Assert.AreNotEqual(0, retval.Id);
+            mockSet.Verify(m => m.Add(It.IsAny<configitem>()), Times.Once());
+            mockContext.Verify(m => m.SaveChanges(), Times.Once());
         }
     }
 }
